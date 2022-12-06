95e99b703593a82de272a6aa692c99d34410ef4e
Irina <kokoladzeirinka@gmail.com>
2022-12-07 03:37:49 +0400

    Add main.cpp file

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char symbol = 'T';
+    int number = 100;
+    float secondNumber = 10.10;
+    double thirdNumber = 50.23;
+    
+    cout << symbol << endl;
+    cout << number << endl;
+    cout << secondNumber << endl;
+    cout << thirdNumber << endl;
+    
+    
+    int array [5] = {5, 10, 15, 20, 30};
+    int sum = array [0] + array [1] + array [2] + array [3] + array [4];
+    int average = (array [0] + array [1] + array [2] + array [3] + array [4])/5;
+    
+    cout << sum << endl;
+    cout << average << endl;
+    
+
+    return 0;
+}
